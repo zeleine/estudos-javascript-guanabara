efa74aa70aa657f9f3e7acc0515b6fcf48bb9973
Zeleine Limão <104385933+zeleine@users.noreply.github.com>
2023-01-07 21:44:42 -0300

    exercícios

---
+++ CursoJs/aula11/ex009.js
@@ -0,0 +1,7 @@
+var país = 'EUA'
+console.log(`Vivendo em ${país}`)
+if (país == 'brasil') {
+  console.log('Você é brasileiro. Parabéns!')
+} else {
+  console.log('Você é estrangeiro. Que pena!')
+}
